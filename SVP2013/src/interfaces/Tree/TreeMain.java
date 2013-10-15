dd78db240fa6d0191c38be16f66983b56a70e9b4
unknown <m05zam@XP8.math.spbu.ru>
2013-10-15 12:12:14 +0400

    minor task Tree

--- SVP2013/src/interfaces/Tree/TreeMain.java
+++ SVP2013/src/interfaces/Tree/TreeMain.java
@@ -1,5 +1,40 @@
 package interfaces.Tree;
 
-public class TreeMain {
+import interfaces.IVisitor;
 
+public class TreeMain {
+	
+	static int level;
+	static ITree tree;
+	static IVisitor visitor;
+	
+	public static void main(String[] args) {
+		
+		tree.add(1);
+		tree.add(13);
+		tree.add(21);
+		tree.add(5);
+		tree.add(7);
+		tree.add(0);
+		tree.add(-1);
+		tree.add(10);
+		tree.add(null);
+		
+		System.out.println("tree deep: " + tree.getTreeDeep());
+		System.out.println("number of nodes on level " + level + ": " + tree.getNumberOfNode(level));
+		
+		System.out.println(" ---- contains ---- ");
+		System.out.println(tree.contains(7));
+		System.out.println(tree.contains(100));
+
+		System.out.println(" ---- remove ---- ");
+		System.out.print(tree.contains(13) + "  remove(13)  " );
+		tree.remove(13);
+		System.out.println(tree.contains(13));
+		System.out.println("tree deep: " + tree.getTreeDeep());
+		System.out.println("number of nodes on level " + level + ": " + tree.getNumberOfNode(level));
+				
+		System.out.println(" ---- visitor ---- ");
+		tree.goLeftTree(visitor);
+	}
 }
