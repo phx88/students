f9339d9080e2f8e0750cdc9c618463c5e3e3f082
Chugunov Vladimir <inflamepheonix@gmail.com>
2013-10-16 23:14:34 +0400

    My tree implementation

---
+++ SVP2013/src/vladimir/chugunov/BinaryTree.java
@@ -0,0 +1,267 @@
+package vladimir.chugunov;
+
+import interfaces.IVisitor;
+
+import java.util.ConcurrentModificationException;
+
+public class BinaryTree implements interfaces.Tree.ITree {
+
+	/**
+	 * cache
+	 */
+	private int	depth;
+
+	private static class Node {
+		Integer	value;
+		Node	left;
+		Node	right;
+
+		public Node(Node l, Node r, Integer v) {
+			value = v;
+			left = l;
+			right = r;
+		}
+
+		public void createSibling(Integer v) {
+			if (v == null || v < value) {
+				left = new Node(null, null, v);
+			} else {
+				right = new Node(null, null, v);
+			}
+		}
+
+		public String toString() {
+			return value == null ? "null" : value.toString();
+		}
+
+		public Integer removeLeaf(Node node) {
+			if (left != null && left.equals(node)) {
+				if (left.left != null || left.right != null) { throw new IllegalStateException(); }
+				Integer retVal = left.value;
+				left = null;
+				return retVal;
+			} else if (right != null && right.equals(node)) {
+				if (right.left != null || right.right != null) { throw new IllegalStateException(); }
+				Integer retVal = right.value;
+				right = null;
+				return retVal;
+			}
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private Node	root;
+
+	@Override
+	public void add(Integer value) {
+		depth = -1;
+		if (root == null) {
+			root = new Node(null, null, value);
+		} else {
+			getNodeToAdd(value).createSibling(value);
+		}
+	}
+
+	private Node getNodeToAdd(Integer value) {
+		Node p1 = root;
+		Node p2 = null;
+		while (p1 != null) {
+			p2 = p1;
+			if (value == null || value < p1.value) {
+				p1 = p1.left;
+			} else {
+				p1 = p1.right;
+			}
+		}
+		return p2;
+	}
+
+	@Override
+	public boolean contains(Integer value) {
+		Node node = root;
+		while (node != null) {
+			if (node.value == value) {
+				return true;
+			} else {
+				node = (value == null || value < node.value) ? node.left : node.right;
+			}
+		}
+		return false;
+	}
+
+	@Override
+	public boolean remove(Integer value) {
+		Node parent = null;
+		Node node = root;
+		while (node != null) {
+			if (node.value == value) {
+				removeNode(node, parent);
+				depth = -1;
+				return true;
+			} else {
+				parent = node;
+				node = (value == null || value < node.value) ? node.left : node.right;
+			}
+		}
+		return false;
+	}
+
+	private Integer removeNode(Node node, Node parent) {
+		if (node.left != null) {
+			Node n = node.left;
+			Node nParent = node;
+			while (n.right != null) {
+				nParent = n;
+				n = n.right;
+			}
+			Integer retVal = node.value;
+			node.value = removeNode(n, nParent);
+			return retVal;
+		} else if (node.right != null) {
+			Node n = node.right;
+			Node nParent = node;
+			while (n.left != null) {
+				nParent = n;
+				n = n.left;
+			}
+			Integer retVal = node.value;
+			node.value = removeNode(n, nParent);
+			return retVal;
+		} else if (parent == null) {
+			if (node != root) { throw new IllegalArgumentException(); }
+			Integer retVal = root.value;
+			root = null;
+			return retVal;
+		} else {
+			return parent.removeLeaf(node);
+		}
+	}
+
+	@Override
+	public int depth() {
+		if (depth < 0) {
+			depth = getDepth(root);
+		}
+		return depth;
+	}
+
+	public int getDepth(Node n) {
+		if (n == null) { return 0; }
+
+		int l = getDepth(n.left);
+		int r = getDepth(n.right);
+		return Math.max(l, r) + 1;
+	}
+
+	private int	levelCounter;
+
+	@Override
+	public synchronized int getSizeOfLevel(int level) {
+		levelCounter = 0;
+		getLevelWidth(root, level, 0);
+		return levelCounter;
+	}
+
+	private void getLevelWidth(Node n, int level, int currDepth) {
+		if (n == null) {
+			return;
+		} else if (level == currDepth) {
+			levelCounter++;
+		} else {
+			if (n.left != null) {
+				getLevelWidth(n.left, level, currDepth + 1);
+			}
+			if (n.right != null) {
+				getLevelWidth(n.right, level, currDepth + 1);
+			}
+		}
+	}
+
+	@Override
+	public synchronized void depthFirstVisiting(IVisitor visitor) {
+		dfs(root, visitor);
+	}
+
+	private void dfs(Node n, IVisitor visitor) {
+		visitor.visit(n);
+		if (n.left != null) {
+			dfs(n.left, visitor);
+		}
+		if (n.right != null) {
+			dfs(n.right, visitor);
+		}
+	}
+
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		depth();// cache depth value
+		printer(sb, root, 0);
+		return sb.toString();
+	}
+
+	private void printer(StringBuilder sb, Node n, int currDepth) {
+		if (depth == -1) { throw new ConcurrentModificationException(); }
+
+		if (n.left != null) {
+			printer(sb, n.left, currDepth + 1);
+		}
+
+		for (int i = 0; i < 2 * (depth - currDepth - 1); i++) {
+			sb.append(' ');
+		}
+		sb.append(n.value);
+		sb.append("——\n");
+
+		if (n.right != null) {
+			printer(sb, n.right, currDepth + 1);
+		}
+	}
+
+	public static void main(String[] args) {
+		final BinaryTree tree = new BinaryTree();
+		 int level = 7;
+		 IVisitor visitor = new Printer();
+		
+		 tree.add(1);
+		 tree.add(13);
+		 tree.add(21);
+		 tree.add(5);
+		 tree.add(7);
+		 tree.add(0);
+		 tree.add(-1);
+		 tree.add(10);
+		 tree.add(8);
+		 tree.add(9);
+		 tree.add(null);
+		
+		
+		 System.out.println("tree depth: " + tree.depth());
+		 System.out.println("number of nodes on level " + level + ": " +
+		 tree.getSizeOfLevel(level));
+		
+		 System.out.println(" ---- contains ---- ");
+		 System.out.println(tree.contains(7));
+		 System.out.println(tree.contains(100));
+		
+		 System.out.println(" ---- remove ---- ");
+		 System.out.print(tree.contains(13) + "  remove(13)  " );
+		 tree.remove(13);
+		 System.out.println(tree.contains(13));
+		 System.out.println("tree depth: " + tree.depth());
+		 System.out.println("number of nodes on level " + level + ": " +
+		 tree.getSizeOfLevel(level));
+		
+		 System.out.println(" ---- visitor ---- ");
+		 tree.depthFirstVisiting(visitor);
+	}
+
+	public static final class Printer implements IVisitor {
+
+		@Override
+		public void visit(Object obj) {
+			System.out.println(obj);
+		}
+
+	};
+
+}
