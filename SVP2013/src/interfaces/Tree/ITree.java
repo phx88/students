b71bf1084c1a8cddd7586cf89d022f6902cac15c
Chugunov Vladimir <inflamepheonix@gmail.com>
2013-10-16 05:10:51 +0400

    A more correct naming and better comments

--- SVP2013/src/interfaces/Tree/ITree.java
+++ SVP2013/src/interfaces/Tree/ITree.java
@@ -3,24 +3,46 @@ package interfaces.Tree;
 import interfaces.IVisitor;
 
 public interface ITree {
-	
-	public void add(Integer value);
-	public boolean contains(Integer value);
-	public boolean remove(Integer value);
-		
-	/**
-	 * @return deep of this tree.
-	 */
-	public int getTreeDeep();
 
 	/**
-	 * @param level
-	 * @return number of nodes on <code>level</code>
+	 * Assigns some node to specified value and adds it to the tree
+	 * 
+	 * @param value
 	 */
-	public int getNumberOfNode(int level);
-	
+	public void add(Integer value);
+
 	/**
-	 * @param visitor 
+	 * @param value
+	 * @return true if on of the nodes of the tree already assigned to specified value
 	 */
-	public void goLeftTree(IVisitor visitor); 
+	public boolean contains(Integer value);
+
+	/**
+	 * Removes from the tree (first met) node which is assigned to specified value
+	 * 
+	 * @param value
+	 * @return false if there is no such node
+	 */
+	public boolean remove(Integer value);
+
+	/**
+	 * @return maximum value of length between root-node and any other stored in this tree
+	 */
+	public int depth();
+
+	/**
+	 * Counts nodes lying in specified level of the tree. "Level" means a set of nodes with similiar
+	 * length (depth) from the root-node
+	 * 
+	 * @param level
+	 * @return amount of nodes in this level
+	 */
+	public int getSizeOfLevel(int level);
+
+	/**
+	 * Visits every node in this tree (using DFS algorithm) and acts on it.
+	 * 
+	 * @param visitor
+	 */
+	public void depthFirstVisiting(IVisitor visitor);
 }
